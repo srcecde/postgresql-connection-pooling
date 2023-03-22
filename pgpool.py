783491bbc2ac84c8a07569db8d27405dd1a848a7
Srce Cde <chiragr83@gmail.com>
2023-03-22 21:33:32 +0530

    added-connection-expiry

--- pgpool.py
+++ pgpool.py
@@ -1,20 +1,26 @@
+import time
 import psycopg2
 from psycopg2 import Error as PoolError
 from psycopg2 import extensions as _ext
 
 
 class ConnectionPool:
-    def __init__(self, min_size=0, **kwargs):
+    def __init__(self, min_size=5, con_min_life_ts=3600, **kwargs):
         self.kwargs = kwargs
         self._min_size = min_size
         self._pool = []
         self._track_conn_in_use = {}
+        self.con_min_life_ts = con_min_life_ts
+        self.track_connection_life_ts = {}
 
         if self._min_size > 5:
             PoolError("Minimum pool size should be < 5")
 
+        if con_min_life_ts > 3600:
+            self.con_min_life_ts = 3600
+
         for _ in range(self._min_size):
-            conn = self.connect()
+            conn = self._connect()
             self._pool.append(conn)
 
     def _connect(self):
@@ -26,7 +32,7 @@ class ConnectionPool:
                 port=self.kwargs.get("port"),
                 dbname=self.kwargs.get("dbname"),
             )
-            print("Connection type", type(conn))
+            self.track_connection_life_ts[id(conn)] = int(time.time())
             return conn
         except:
             PoolError("Invalid DB credentials")
@@ -43,16 +49,23 @@ class ConnectionPool:
 
     def close(self, conn):
         if not conn.closed:
-            status = conn.info.transaction_status
-            if status == _ext.TRANSACTION_STATUS_UNKNOWN:
+            if (
+                self.con_min_life_ts > 0
+                and int(time.time()) - self.track_connection_life_ts.get(id(conn))
+                > self.con_min_life_ts
+            ):
                 conn.close()
-            elif status != _ext.TRANSACTION_STATUS_IDLE:
-                conn.rollback()
-                self._pool.append(self._track_conn_in_use[id(conn)])
-                del self._track_conn_in_use[id(conn)]
             else:
-                self._pool.append(self._track_conn_in_use[id(conn)])
-                del self._track_conn_in_use[id(conn)]
+                status = conn.info.transaction_status
+                if status == _ext.TRANSACTION_STATUS_UNKNOWN:
+                    conn.close()
+                elif status != _ext.TRANSACTION_STATUS_IDLE:
+                    conn.rollback()
+                    self._pool.append(self._track_conn_in_use[id(conn)])
+                    del self._track_conn_in_use[id(conn)]
+                else:
+                    self._pool.append(self._track_conn_in_use[id(conn)])
+                    del self._track_conn_in_use[id(conn)]
 
     def closeall(self) -> None:
         if self._pool:
